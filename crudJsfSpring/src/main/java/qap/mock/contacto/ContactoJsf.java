c33740903e703e3c2aeb935018fe8e8a61cdbf38
sieteideas <ch.roig@gmail.com>
2017-07-31 15:04:14 -0300

    Separa clase ContactoMock

--- crudJsfSpring/src/main/java/qap/mock/contacto/ContactoJsf.java
+++ crudJsfSpring/src/main/java/qap/mock/contacto/ContactoJsf.java
@@ -13,13 +13,13 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean(name="contactos") // creado en faces-config
 @SessionScoped
-public class ContactoMockJsf implements Serializable {
+public class ContactoJsf implements Serializable {
     // lista
     private final CrudFilterOptions filter;
     private List items;
     private Contacto selected;
 
-    public ContactoMockJsf() {
+    public ContactoJsf() {
         this.filter = filterOptions();
     }
 
@@ -29,7 +29,7 @@ public class ContactoMockJsf implements Serializable {
 
     public List<Contacto> getItems() {
         if (items == null) {
-            items = mockList("demo", "Alumnos");
+            items = ContactoMock.mockList("demo", "Alumnos");
             //items = svc.getItems("demo", "Alumnos");
         }
         return items;
@@ -47,28 +47,6 @@ public class ContactoMockJsf implements Serializable {
         //nada x ahora
     }
 
-    // mock
-    static public List mockList(String propietario, String modulo) {
-        List l = new ArrayList();
-        
-        l.add(new Contacto(1, propietario, modulo, "024", "Perez, Juan Pablo"));
-        l.add(new Contacto(2, propietario, modulo, "031", "Ferrari, Daniela"));
-        l.add(new Contacto(3, propietario, modulo, "045", "Roig, Christian"));
-        l.add(new Contacto(4, propietario, modulo, "052", "Rodriguez, Emmanuel"));
-        l.add(new Contacto(5, propietario, modulo, "053", "Fracchia, Marcos"));
-        l.add(new Contacto(6, propietario, modulo, "055", "Cao, Santiago"));
-        
-        if ("Clientes".equals(modulo)) {
-            l.add(new Contacto(7, propietario, modulo, "064", "Clinica de Ojos S.A."));            
-        }
-        if ("Proveedores".equals(modulo)) {
-            l.add(new Contacto(8, propietario, modulo, "065", "Telecom S.A."));            
-            l.add(new Contacto(9, propietario, modulo, "065", "Camuzzi Gas Pampeana S.A."));            
-        }
-
-        return l;
-    }
-    
     // filter
     static public CrudFilterOptions filterOptions() {
         CrudFilterOptions filter = new CrudFilterOptions();
